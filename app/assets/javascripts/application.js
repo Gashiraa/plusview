eb8f64d5712f3bc41b43924ccc5ec37f2c14551d
Gashira <j.hugues.guinot@gmail.com>
2019-03-01 05:18:43 +0100

    Models & Wares FE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,34 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require_tree .
+
+
+$(document).on("turbolinks:load",function() {
+    $("tr[data-link]").click(function() {
+        window.location = $(this).data("link")
+    })
+
+    $('th').click(function(){
+        var table = $(this).parents('table').eq(0)
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+        if (!this.asc){rows = rows.reverse()}
+        for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+    })
+    function comparer(index) {
+        return function(a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
+    }
+    function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
+    $("#notice").onshow(function(){
+        console.log('works');
+    })
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :admins
+  resources :wares
+  resources :services
+  resources :projects
+  resources :customers
+  resources :invoices
+  resources :payments
+  resources :quotations
+
+  root to: 'wares#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
