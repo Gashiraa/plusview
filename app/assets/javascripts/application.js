3127353f837990ef2dd018adac24848db1cd3bfc
Jean-Hugues Guinot <j.hugues.guinot@gmail.com>
2019-03-02 12:11:48 +0100

    Clickable row/links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,12 +22,16 @@
 $(document).on("turbolinks:load", function () {
     $("tr[data-link]").click(function () {
         window.location = $(this).data("link")
-    })
+    });
+
+    // $("tr[data-link] td a").click(function () {
+    //     event.stopPropagation();
+    // });
 
     $('th').click(function () {
-        var table = $(this).parents('table').eq(0)
-        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
-        this.asc = !this.asc
+        var table = $(this).parents('table').eq(0);
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+        this.asc = !this.asc;
         if (!this.asc) {
             rows = rows.reverse()
         }
@@ -38,7 +42,7 @@ $(document).on("turbolinks:load", function () {
 
     function comparer(index) {
         return function (a, b) {
-            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            var valA = getCellValue(a, index), valB = getCellValue(b, index);
             return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
         }
     }
@@ -51,9 +55,28 @@ $(document).on("turbolinks:load", function () {
         $('#notice').fadeOut();
     }, 3000);
 
+    $('select[name="project_id"]').change(function () {
+        if ($(this).val().length > 0) {
+            $('#customer_id').prop("disabled", true);
+        } else {
+            $('#customer_id').prop("disabled", false);
+        }
+    });
+    $('select[name="customer_id"]').change(function () {
+        if ($(this).val().length > 0) {
+            $('#project_id').prop("disabled", true);
+        } else {
+            $('#project_id').prop("disabled", false);
+        }
+    });
+    $(document).ready(function() {
+        $.each($('.navbar-nav').find('li'), function() {
+            $(this).toggleClass('active',
+                window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
+        });
+    });
 });
 
 $(document).ready(function () {
-
     }
 );
