6cb42dcdcb5a016f44ce0de5cad8594854c05b70
Jean-Hugues Guinot <j.hugues.guinot@gmail.com>
2019-03-11 18:32:31 +0100

    Clickable row/links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,12 +69,38 @@ $(document).on("turbolinks:load", function () {
             $('#project_id').prop("disabled", false);
         }
     });
-    $(document).ready(function() {
-        $.each($('.navbar-nav').find('li'), function() {
+    $(document).ready(function () {
+        $.each($('.navbar-nav').find('li'), function () {
             $(this).toggleClass('active',
                 window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
         });
     });
+
+    $('#waresForm,#total_cost,#quantity,#unit_price,#tva_rate,#margin').on('keyup keypress mouseover change', function () {
+        let total_cost = document.getElementById('total_cost');
+        let quantity = document.getElementById('quantity').value || 0;
+        let unit_price = document.getElementById('unit_price').value || 0;
+        let margin = document.getElementById('margin').value || 1;
+        let tva_rate = document.getElementById('tva_rate').value || 0;
+
+        let total = ((parseInt(quantity) * parseFloat(unit_price)) * parseFloat(margin)) * (1 + parseFloat(tva_rate) / 100);
+        total_cost.value = total.toFixed(2);
+    });
+
+    $('#servicesForm,#total_cost_s,#hourly_rate,#tva_rate_s,#coefficient,#_duration_4i,#_duration_5i').on('keyup keypress mouseover change', function () {
+        let total_cost = document.getElementById('total_cost_s');
+        let hourly_rate = document.getElementById('hourly_rate').value || 0;
+        let coefficient = document.getElementById('coefficient').value || 1;
+        let tva_rate = document.getElementById('tva_rate_s').value || 0;
+        let hours = document.getElementById('_duration_4i').value || 0;
+        let minutes = document.getElementById('_duration_5i').value || 0;
+
+
+        let total = (((parseInt(hours) + parseFloat(minutes)/60) * parseFloat(hourly_rate)) * parseFloat(coefficient)) * (1 + parseFloat(tva_rate) / 100);
+        total_cost.value = total.toFixed(2);
+    });
+
+
 });
 
 $(document).ready(function () {
